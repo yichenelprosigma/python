7c1e6524ca7239fdae1ad565919e0b134090adbd
yichen <yichen7shaohola@gmail.com>
2025-12-22 16:42:33 +0000

    tarea 11 corregido

---
+++ Clase_11_operadores.py
@@ -0,0 +1,36 @@
+#Las operadores de python 
+
+a=1
+b=2
+c=3
+is_open=True
+#si es a igual que b
+if(a ==  b):
+    print("hello world")
+
+#cumplir dos condiciones- and &
+if (a==b) and (a==c):
+    print("hello world2")
+# cumplir una de las dos condiciones-or ||
+if (a==b) or (a==c):
+    print("hello world3")
+# comprobar el contrario- not -Para booleanos
+if not is_open:
+    print("hello world4")
+#comparar dos variables son iguales - operador ==
+if (a == b):
+    print("a es igual a b")
+#comparar dos variables son diferentes - operador !=
+if (a != b):
+    print("a es diferente a b")
+
+# >
+#<
+#<=
+#>=
+# +
+# -
+# *
+# /
+# % (modulo) 10-3=3 9 resto=1
+# ** potencia 2**3=8
--- tarea13.py
+++ tarea13.py
@@ -9,43 +9,46 @@ MatrizA = [
 MatrizB = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9],
 ]
 
 # Paso 1: Imprimir las dos matrices
-print("Matriz A:")
-for fila in MatrizA:
-    print(fila)
+# print("Matriz A:")
+# for fila in MatrizA:
+#     print(fila)
 
-print("\nMatriz B:")
-for fila in MatrizB:
-    print(fila)
+# print("\nMatriz B:")
+# for fila in MatrizB:
+#     print(fila)
 
-# Paso 2: Transpuesta de las matrices
-Matriz_TraspuestaA=[[0 for j in range(len(MatrizA[0]))] for i in range(len(MatrizA))]
-for i in range(len(MatrizA)):
-    for j in range(len(MatrizA[0])):
-       Matriz_TraspuestaA[j][i]=MatrizA[i][j]
-Matriz_TraspuestaB=[[0 for j in range(len(MatrizB[0]))] for i in range(len(MatrizB))]
-for i in range(len(MatrizB)):
-    for j in range(len(MatrizB[0])):
-       Matriz_TraspuestaB[j][i]=MatrizB[i][j]
-print("\nTranspuesta de la Matriz A:")
-for fila in Matriz_TraspuestaA:
-    print(list(fila))
+# # Paso 2: Transpuesta de las matrices
+# Matriz_TraspuestaA=[[0 for j in range(len(MatrizA[0]))] for i in range(len(MatrizA))]
+# for i in range(len(MatrizA)):
+#     for j in range(len(MatrizA[0])):
+#        Matriz_TraspuestaA[j][i]=MatrizA[i][j]
+# Matriz_TraspuestaB=[[0 for j in range(len(MatrizB[0]))] for i in range(len(MatrizB))]
+# for i in range(len(MatrizB)):
+#     for j in range(len(MatrizB[0])):
+#        Matriz_TraspuestaB[j][i]=MatrizB[i][j]
+# print("\nTranspuesta de la Matriz A:")
+# for fila in Matriz_TraspuestaA:
+#     print(list(fila))
 
-print("\nTranspuesta de la Matriz B:")
-for fila in Matriz_TraspuestaB:
-    print(list(fila))
+# print("\nTranspuesta de la Matriz B:")
+# for fila in Matriz_TraspuestaB:
+#     print(list(fila))
 
-# Paso 3: Suma de las dos matrices
-Suma = []
-for i in range(len(MatrizA)):
-    fila = []
-    for j in range(len(MatrizA[0])):
-        fila.append(MatrizA[i][j] + MatrizB[i][j])
-    Suma.append(fila)
+if len(MatrizA)==len(MatrizB) and len(MatrizA[0])==len(MatrizB[0]):
 
-print("\nSuma de la Matriz A y Matriz B:")
-for fila in Suma:
-    print(fila)
+    # Paso 3: Suma de las dos matrices
+    Suma = []
+    for i in range(len(MatrizA)):
+        fila = []
+        for j in range(len(MatrizA[0])):
+            fila.append(MatrizA[i][j] + MatrizB[i][j])
+        Suma.append(fila)
+
+    print("\nSuma de la Matriz A y Matriz B:")
+    for fila in Suma:
+        print(fila)
+else:
+    print("no se puede hacer esta suma")
