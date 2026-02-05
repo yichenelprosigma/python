c0b7478bdaffb99f69fefbfb3b5bf85148b442f9
Yichen <yichen7shaohola@gmail.com>
2026-02-05 16:41:46 +0000

    tarea17 corregida

--- problema1.py
+++ problema1.py
@@ -1,6 +1,7 @@
 def cm_a_pulgadas(cm):
-    return cm / 2.54
+    return cm / 100
 
 longitud = float(input("Introduzca longitud: "))
 resultado = cm_a_pulgadas(longitud)
-print(f"{longitud} cm = {resultado:.2f} in")
+#imprimir la longitud que es 
+print(f"{longitud} cm = {resultado:.2f} m")
