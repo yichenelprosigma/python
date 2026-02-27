3decf5960a305e96eb93af7b568afb3427fa7046
yichen <yichen7shaohola@gmail.com>
2026-02-27 17:02:32 +0000

    tarea 19

--- clase_13_recursivo.py
+++ clase_13_recursivo.py
@@ -26,3 +26,21 @@ def fibonacci (n):
         return fibonacci(n-1) + fibonacci (n-2)
 
 print(fibonacci(7))
+
+def sum_list(numbers):
+    if len(numbers) == 0:
+        return 0
+    else:
+        return numbers[0] + sum_list(numbers[1:])
+my_list= [1,2,3,4,5,6]
+print(sum_list(my_list))
+#1.explicar la función , y dime los parámetros de cada vez cuando llama la función
+# problema6.py
+# Tarea: Desarrolle en python un programa que reciba como entrada las longitudes de los dos catetos de un
+# triángulo rectángulo, y que calcule como salida la longitud de la hipotenusa del triángulo, dado por el
+# teorema de Pitágoras. Necesitará para ello math.sqrt(), import math (importacion de la libreria)
+# Introduzca cateto a: 7
+# Introduzca cateto b: 5 
+# El valor de la hipotenusa es 8.60
+
+# c1 ^ 2 + c2 ^ 2 = hipo ^ 2
