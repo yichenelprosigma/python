fab2f2066b5049788acc1832fd346feebeb761b7
yichen <yichen7shaohola@gmail.com>
2026-03-27 16:29:52 +0000

    problema7

--- problema7.py
+++ problema7.py
@@ -1,7 +1,22 @@
-#una función con cuatro parámetros 
+import math
+# #una función con cuatro parámetros 
 
-def polinomio(numeroa,numerob,numeroc,x):
-    x=100
-    ecuación=numeroa*x**2+numerob*x+numeroc
-    print(ecuación)
-polinomio(2,3.7,-0,5)
+# def polinomio(numeroa,numerob,numeroc,x):
+#     ecuación=numeroa*x**2+numerob*x+numeroc
+#     print(ecuación)
+    
+# polinomio(2,3.7,-0.5,100)
+
+#modificación
+def polinomio(numeroa,numerob,numeroc):
+    if math.sqrt(numerob**2-4*numeroa*numeroc) <0:
+        print("No se puede hacer esta ecuación")
+    else:
+        ecuación1=(-numerob+math.sqrt(numerob**2-4*numeroa*numeroc))/2*numeroa
+        ecuación2=(-numerob-math.sqrt(numerob**2-4*numeroa*numeroc))/2*numeroa
+        print(int(ecuación1))
+        print(int(ecuación2))
+        #print(f"{numero:.0f}"
+        #print(f"{numero:.0f}"
+        
+polinomio(1,1,1)
