096e2cc5916a5a7f2d29e0e579fba90bdafec54f
yichen <yichen7shaohola@gmail.com>
2026-03-28 13:46:45 +0000

    problema 8

--- problema8.py
+++ problema8.py
@@ -1,1 +1,7 @@
-#guardar los números en una lista, finalmente imprimir la lista f
+#guardar los números en una lista, finalmente imprimir la lista 
+def potencias(n):
+    lista=[]
+    for i in range(n + 1):
+        lista.append(2 ** i)
+    print(lista)
+potencias(10)
